3a04535b6ff88315b1f17c60baa3db417b03956b
Erich <peqoud@gmail.com>
2023-01-13 18:58:09 +0100

    Update iobroker_lights_2.js

--- iobroker_lights_2.js
+++ iobroker_lights_2.js
@@ -8,24 +8,67 @@ Ideen:
 - 
 */
 
-
-let lights = {
-	light1: 'homematic.0.devices.YourDeviceId.CHANNEL.STATE',
-	light2: 'homematic.0.devices.YourDeviceId.CHANNEL.STATE',
-	light3: 'homematic.0.devices.YourDeviceId.CHANNEL.STATE',
-	light4: 'homematic.0.devices.YourDeviceId.CHANNEL.STATE',
+/*
+States for HUE lights:
++) action.on
++) action.brightness
++) action.level
+*/
+class huelight {
+  constructor(id, brightness = 20, level = 0) {
+    this.id         = id;
+    this.brightness = brightness;
+    this.level      = level;
+  }
+  
+  setSwitchLight(status)
+  {
+    setState(this.id+'.action.on'), status);
+    log(this.id+'.action.on = '+ status);
+  }
+  
+  setBrightness(level = this.brightness)
+  {
+    setState(this.id+'.action.brightness'), level);
+    log(this.id+'.action.brightness = '+ status);
+  }
+  
+  setLevel(level = this.level)
+  {
+    setState(this.id+'.action.level'), level);
+    log(this.id+'.action.level = '+ status);
+  }
 }
+
+/* Array of lights */
+let lights = {
+	light1: new huelight('hue-extended.0.lights.035-außen_weg_1'   , 20),
+	light2: new huelight('hue-extended.0.lights.002-aussen_weg_2'  , 20),
+	light3: new huelight('hue-extended.0.lights.001-aussen_weg_3'  , 20)
+}
+
+let LightOnTimeSeconds = 60;  // Time in Seconds
+let LightOnTimeMs = LightOnTimeSeconds * 1000;
+
 
 // Configuration which lights to switch on
 let MotionLightConfig = {
-	MotionSensor1Lights: ['light1','light2','light3','light4'],
+	MotionSensor1Lights: ['light1','light2','light3'],
 	MotionSensor2Lights: ['light1','light2']
 }
 
 // Bewegungsmelder und Helligkeitssensor Zustände definieren
-let motionSensor1    = 'homematic.0.devices.YourDeviceId.CHANNEL.STATE';
-let motionSensor2    = 'homematic.0.devices.YourDeviceId.CHANNEL.STATE';
-let brightnessSensor = 'homematic.0.devices.YourDeviceId.CHANNEL.STATE';
+/*
+States:
++) .MOTION
++) .MOTION_DETECTION_ACTIVE
++) .ILLUMINATION
++) .CURRENT_ILLUMINATION
+
+*/
+let motionSensor1    = 'hm-rpc.0.000BD569A36E1D.1';
+//let motionSensor2    = 'homematic.0.devices.YourDeviceId.CHANNEL.STATE';
+//let brightnessSensor = 'homematic.0.devices.YourDeviceId.CHANNEL.STATE';
 
 // MotionSensor
 let brightnessThreshold = 10;
@@ -33,14 +76,7 @@ let brightnessThreshold = 10;
 // Zeitfenster (in 24-Stunden-Format)
 let MotionSensorActiveStartTime         = '17:00:00'; // Alternative - Astro mit Sonnenuntergang nutzen
 let MotionSensorActiveEndTime           = '23:00:00';
-
-// Light Configuration
-let desiredBrightness  = 20;
-let LightOnTimeSeconds = 60;  // Time in Seconds
-let LightOnTimeMs = LightOnTimeSeconds * 1000
-
-// Timer configuration
-timerLightsOn = 'timers.lighttimer'
+var TimerOn = null; // Handler of the Timeout
 
 
 // Function that turn ON lights
@@ -48,7 +84,8 @@ function turnOnLight(light_ids)
 {
 	for (const light of light_ids){
         log('Turn on ' + light);
-		setState(lights[light_id], {brightness: 100});
+		lights[light_id].setBrightness());
+		lights[light_id].setSwitchLight(true);
     }
 }
 
@@ -57,7 +94,7 @@ function turnOffLight(light_id)
 {
 	for (const light of light_ids){
         log('Turn off ' + light);
-		setState(lights[light_id], false);
+		lights[light_id].setSwitchLight(false);
     }
 }
 
@@ -66,8 +103,16 @@ function turnOffAllLight()
 {
 	for (const light of lights){
         log('Turn off ' + light);
-		setState(lights[light], false);
+		lights[light_id].setSwitchLight(false);
     }
+}
+
+
+function TimerOnExpired()
+{
+	TimerOn = null;
+	turnOffAllLight();
+	log('Light Timer expired - Switch of all lights.');
 }
 
 // Generic Function for handling the Sensor and lights
@@ -77,7 +122,8 @@ function MotionHandling(MotionSensor)
 	turnOnLight(MotionLightConfig[MotionSensor]);
 	
 	// Start Common Timer for Lights ON
-	setState(timerLightsOn + '.start', LightOnTimeMs);
+	if (TimerOn != null) {clearTimeout (TimerOn));}  // Reset Timer if active
+	TimerOn = setTimeout(TimerOnExpired(), LightOnTimeMs); // Start Timer for switch off
 	log('Light Timer Started.');
 }
 
@@ -88,7 +134,6 @@ on({id: timerLightsOn + '.action', change: "ne"}, function (obj) {
 		log('Light Timer expired - Switch of all lights.');
     }
 });
-
 
 // Enable MotionDetection at MotionSensorActiveStartTime
 schedule({hour:   MotionSensorActiveStartTime.split(':')[0], 
@@ -103,19 +148,25 @@ schedule({hour:   MotionSensorActiveStartTime.split(':')[0],
 	// Reaching the start time shall check the Sensor input again, to see if movement is going on
 
 	// Motion Event 1 - TODO: On which value shall they trigger??
-	SubID1 = on({id: motionSensor1, change: "any"}, function (obj) {
+	SubID1 = on({id: motionSensor1+'MOTION', change: "any"}, function (obj) {
 		log('MotionDetected Sensor 1');
 		MotionHandling('MotionSensor1Lights');
 		
 	});
+	log("Lights Subscribed to MotionSensors 2");
 
 	// Motion Event 2 - TODO: On which value shall they trigger??
-	SubID2 = on({id: motionSensor2, change: "any"}, function (obj) {
-		log('MotionDetected Sensor 2');
-		MotionHandling('MotionSensor2Lights');
-	});
+	// SubID2 = on({id: motionSensor2, change: "any"}, function (obj) {
+		// log('MotionDetected Sensor 2');
+		// MotionHandling('MotionSensor2Lights');
+	// });
 	
-    log("Lights Subscribed to MotionSensors");
+    // log("Lights Subscribed to MotionSensors 2");
+});
+
+
+schedule({astro: "dusk", shift: 0}, async function () {
+	log("Sonnuntergang");
 });
 
 // Switch OFF all lights and MotionSensorActiveEndTime  AND disable MotionDetection
